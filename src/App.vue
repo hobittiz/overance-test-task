8e11bff47dbd2bbbe1f44181c33743757cd62293
hobittiz <vova@goodface.agency>
2024-05-23 13:29:52 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <form-view />    
+  </main>
+</template>
+
+<script setup lang="ts">
+  import formView from './components/formView.vue'
+</script>
+
+<style scoped>
+
+</style>
